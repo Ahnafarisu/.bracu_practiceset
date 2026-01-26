13a0a7f7e5f7ffc8ac31f54dc6500a0014ef25de
Arisu <alifsiddique.2006@gmail.com>
2026-01-26 17:57:22 +0600

    Add files via upload

---
+++ python_practice2.py
@@ -0,0 +1,14 @@
+a = 5
+b = 3
+c = 5 // 3
+print("The value of c is:", c)
+
+d = 5
+e = 3
+f = 5 / 3 
+print("The value of f is:", f)
+'''
+This is an example of floor division and regular division.
+Which indicates that regular '/' division returns a float value,
+while '//' floor division returns an integer value by discarding the decimal part.
+'''
