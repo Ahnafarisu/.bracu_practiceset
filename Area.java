aaa6fe042c3cc77f688fc4e71da65c761e1bad12
Arisu <alifsiddique.2006@gmail.com>
2026-02-06 02:48:24 +0600

    Add explanatory comment to Area.java
    Add comment explaining variable assignment logic

---
+++ Area.java
@@ -0,0 +1,19 @@
+import java.util.Scanner; //imports scanner to read the user input
+
+public class area {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);  //Defining scanner
+        System.out.println("Enter the value of r: "); //Taking user input
+        double r = scanner.nextDouble(); //scanning the double number
+        double area = 3.14 * r * r;  //Calculation maintaining data variable
+        System.out.println(area);  //printing the desired result.
+        
+                }
+
+
+    }
+}
+
+"""
+  Simple code to understand how logic and assining of variable works
+  """
